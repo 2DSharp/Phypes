5341d9ba1c13d1ceda51f21c5d7f3a32967a0d33
Dedipyaman Das <30053071+2Dsharp@users.noreply.github.com>
2018-08-30 09:56:27 +0530

    Removed constructor from interface, bad practice.
    Although PHP allows this, it's not a good idea to define the
    constructor in the interface since it's an implementation detail. A
    particular implementation shouldn't be enforced upon.

--- src/Type/Type.php
+++ src/Type/Type.php
@@ -6,11 +6,6 @@ use GreenTea\Flux\Validator\Validator;
 
 interface Type
 {
-    /**
-     * Instantiate the Flux data type
-     * @param Validator $validator : A validator implementing the Validator interface
-     */
-    public function __construct($value, Validator $validator);
     public function __toString() : string;
     public function getValue();
 }
