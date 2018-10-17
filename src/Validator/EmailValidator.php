ae662bb3f01f664f61bc6496e2c2886044ea393b
2dsharp <30053071+2Dsharp@users.noreply.github.com>
2018-10-17 20:16:33 +0530

    Add email validation error message getter

--- src/Validator/EmailValidator.php
+++ src/Validator/EmailValidator.php
@@ -10,14 +10,45 @@ declare(strict_types=1);
 
 namespace GreenTea\Phypes\Validator;
 
+use GreenTea\Phypes\Exception\PrematureErrorCallException;
 
 class EmailValidator implements Validator
 {
+    /**
+     * @var string $error
+     * Stores the error message on validation failure
+     */
+    private $error;
+    private $validated = false;
+
     public function isValid($email, $options = []): bool
     {
+        $this->validated = true;
+
         if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $this->error = '';
             return true;
         }
+        $this->error = 'The provided email is invalid.';
         return false;
+    }
+
+    /**
+     * Return the error message upon validation, empty string returned if no errors are set
+     * @return string $error
+     * @throws PrematureErrorCallException
+     */
+    public function getErrorMessage(): string
+    {
+        /**
+         * Extra check to make sure someone doesn't call this method before actually calling isValid
+         */
+        if ($this->validated == false) {
+            throw new PrematureErrorCallException();
+        }
+        if (is_null($this->error)) {
+            return '';
+        }
+        return $this->error;
     }
 }
--- tests/UnitTests/Validator/EmailValidatorTest.php
+++ tests/UnitTests/Validator/EmailValidatorTest.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 
 namespace GreenTea\Phypes\Validator;
 
+use GreenTea\Phypes\Exception\PrematureErrorCallException;
 use PHPUnit\Framework\TestCase;
 
 class EmailValidatorTest extends TestCase
@@ -42,5 +43,47 @@ class EmailValidatorTest extends TestCase
     {
         $result = $this->validator->isValid('12345lol@');
         $this->assertFalse($result);
+        $result = $this->validator->isValid('');
+        $this->assertFalse($result);
+    }
+
+    /**
+     * Check the error message delivered on failure
+     */
+    public function testErrorMessageReturned() : void
+    {
+        $this->validator->isValid('invalid email');
+        $result = $this->validator->getErrorMessage();
+
+        $this->assertEquals('The provided email is invalid.', $result);
+    }
+
+    /**
+     * Make sure the error message is null on valid data
+     */
+    public function testNoErrorMessageOnValidEmail() : void
+    {
+        $this->validator->isValid('2d@twodee.me');
+        $result = $this->validator->getErrorMessage();
+
+        $this->assertEmpty($result);
+    }
+
+    /**
+     * Return the error state to empty on successful validation
+     */
+    public function testWithMultipleValidations() : void
+    {
+        $this->testErrorMessageReturned();
+        $this->testNoErrorMessageOnValidEmail();
+    }
+
+    /**
+     * Expect an exception to be thrown on calling getErrorMessage() before isValid()
+     */
+    public function testExceptionOnPrematureErrorRetrieval() : void
+    {
+        $this->expectException(PrematureErrorCallException::class);
+        $this->validator->getErrorMessage();
     }
 }
