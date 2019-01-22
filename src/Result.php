310558b154d5eacc8d8bef791a5a1d2cbf88430e
2dsharp <30053071+2Dsharp@users.noreply.github.com>
2019-01-23 03:02:44 +0530

    Add Result VO

---
+++ src/Result.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dedipyaman
+ * Date: 1/23/19
+ * Time: 2:07 AM
+ */
+
+namespace Phypes;
+
+use Phypes\Error\Error;
+
+class Result
+{
+    private $valid;
+    private $errors = [];
+
+    public function __construct(bool $valid, $errors = [])
+    {
+        $this->valid = $valid;
+        $this->errors = $errors;
+    }
+
+    public function isValid() : bool
+    {
+        return $this->valid;
+    }
+
+    /**
+     * @return array
+     */
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+
+    public function getFirstError() : Error
+    {
+        return $this->errors[0];
+    }
+}
