0eabc1ee2bcc0b90ab381b819eb632ea6fe3bff4
2dsharp <30053071+2Dsharp@users.noreply.github.com>
2019-06-16 02:49:04 +0530

    Add Username validator utilizing ForAll rule

---
+++ src/Validator/UsernameValidator.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * This file is part of Phypes <https://github.com/2DSharp/Phypes>.
+ *
+ * (c) Dedipyaman Das <2d@twodee.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace Phypes\Validator;
+
+use Phypes\Result\Result;
+use Phypes\Rule\Aggregate\ForAll;
+use Phypes\Rule\String\AlphaNumeric;
+use Phypes\Rule\String\MaximumLength;
+use Phypes\Rule\String\MinimumLength;
+
+class UsernameValidator implements Validator
+{
+    /**
+     * @var integer $minLength
+     */
+    private $minLength;
+    /**
+     * @var integer $maxLength
+     */
+    private $maxLength;
+    /**
+     * @var array $allowedChars
+     */
+    private $allowedSpecialChars = [];
+
+    public function __construct(int $minLength = 4, int $maxLength = 12, $allowedSpecialChars = [])
+    {
+        $this->minLength = $minLength;
+        $this->maxLength = $maxLength;
+        $this->allowedSpecialChars = $allowedSpecialChars;
+    }
+
+    public function validate($username): Result
+    {
+        $rule = new ForAll(
+            new AlphaNumeric($this->allowedSpecialChars),
+            new MinimumLength($this->minLength),
+            new MaximumLength($this->maxLength));
+
+        return $rule->validate($username);
+
+    }
+}
