0ca5fc59d582dcd2d67359fe328665875d6d58ae
2dsharp <30053071+2Dsharp@users.noreply.github.com>
2019-06-15 17:42:42 +0530

    Add generic abstract class to allow optional special chars

---
+++ src/Rule/String/StringTypes.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This file is part of Phypes <https://github.com/2DSharp/Phypes>.
+ *
+ * (c) Dedipyaman Das <2d@twodee.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace Phypes\Rule\String;
+
+
+abstract class StringTypes
+{
+    /**
+     * Special characters excused within the string pattern
+     * Handy for username validations
+     * @var array $allowedSpecialChars
+     */
+    protected $allowedSpecialChars;
+
+    /**
+     * AlphaNumeric constructor.
+     * @param array $allowedSpecialChars
+     */
+    public function __construct($allowedSpecialChars = [])
+    {
+        $this->allowedSpecialChars = $allowedSpecialChars;
+    }
+}
