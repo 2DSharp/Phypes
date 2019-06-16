5c40cfdfbc9de90c3ecda71415e48bdc900e1487
2dsharp <30053071+2Dsharp@users.noreply.github.com>
2019-06-16 13:55:30 +0530

    Add exception for aggregate rules

---
+++ src/Exception/InvalidAggregateRule.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file is part of Phypes <https://github.com/2DSharp/Phypes>.
+ *
+ * (c) Dedipyaman Das <2d@twodee.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Phypes\Exception;
+
+final class InvalidAggregateRule extends \Exception
+{
+    public function __construct(string $message, string $aggregateClass)
+    {
+        parent::__construct($aggregateClass . ":" . $message);
+    }
+}
